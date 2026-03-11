6468d9aaa040af811d99bbbbf195f9cb7c9a9d2e
washedfs <182323881+washedfs@users.noreply.github.com>
2026-03-11 17:06:04 -0400

    Update __init__.py

--- wapfunc/__init__.py
+++ wapfunc/__init__.py
@@ -1,7 +1,8 @@
-from . import aniplus, audioutil, bdmv, subutil
-
+from .aniplus import *
 from .audioutil import *
 from .bdmv import *
 from .subutil import *
 
-__all__ = ['handle_audio', 'm2ts_from_playlist', 'm2ts_from_playlists', 'create_signs_track', 'restyle_dialogue', 'set_layoutres']
+__all__ = [
+    'handle_signs', 'fix_dashes', 'handle_audio', 'm2ts_from_playlist', 'm2ts_from_playlists', 'create_signs_track', 'restyle_dialogue', 'set_layoutres', 'add_line', 'add_syncpoint'
+]
