0db43147c6d9fae16c5cc404bb32a08ddb1969c6
washedfs <182323881+washedfs@users.noreply.github.com>
2026-03-11 18:18:41 -0400

    Update __init__.py

--- wapfunc/__init__.py
+++ wapfunc/__init__.py
@@ -4,5 +4,5 @@ from .bdmv import *
 from .subutil import *
 
 __all__ = [
-    'handle_signs', 'fix_dashes', 'handle_audio', 'm2ts_from_playlist', 'm2ts_from_playlists', 'create_signs_track', 'restyle_dialogue', 'set_layoutres', 'add_line', 'add_syncpoint'
+    'handle_signs', 'fix_dashes', 'handle_audio', 'm2ts_from_playlist', 'm2ts_from_playlists', 'locate_playlist', 'create_signs_track', 'restyle_dialogue', 'set_layoutres', 'add_line', 'add_syncpoint'
 ]
